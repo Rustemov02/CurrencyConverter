206a99272a83b2fb27716bbbf8661c0ed139c169
Rustemov02 <bunyamin2002@mail.ru>
2022-11-22 00:22:08 +0400

    added

--- script.js
+++ script.js
@@ -10,30 +10,20 @@ const gbpBtn = document.querySelector('.gbp')
 
 let baseParagraph = document.querySelector('.baseParagraph')
 let rateParagraph = document.querySelector('.rateParagraph')
-   //               base , rate => string       base,rateInput = number             
-let obj = { base: null, rate: null, baseInput: 0, rateInput: 0 }, rate, base ,reValue = 0 , baseAmount
+let obj = { base: null, rate: null, sum: null, baseInput: 0, rateInput: 0 }, rate
 
 window.addEventListener('load', () => {
     baseButton[0].classList.add('active')
     rateButton[1].classList.add('active')
     obj.base = baseButton[0].innerHTML
+    obj.sum = baseButton[0].innerHTML
     obj.rate = rateButton[1].innerHTML
 })
 
 baseInput.addEventListener('keyup', () => {
     obj.baseInput = baseInput.value
-    main()
-})
-rateInput.addEventListener('keyup' , ()=>{
-    obj.rateInput = rateInput.value  
-    // obj.rate = USD
-    // obj.base = RUB
-    // obj.baseInput = sol teref inputu
-    // obj.rateInpur = sagh teref inputu 
-    reValue = obj.baseInput * obj.rateInput 
-    //ters function uchun reValue / rate(getFetch daxilindeki ) olmalidir
-})
-
+    getFetch(obj)
+}) 
 baseButton.forEach((event) => {
     event.addEventListener('click', function () {
         baseButton.forEach((event) => {
@@ -41,7 +31,7 @@ baseButton.forEach((event) => {
             this.classList.add('active')
         })
         obj.base = this.innerText
-        main()
+        getFetch(obj)
     })
 })
 
@@ -52,43 +42,25 @@ rateButton.forEach((event) => {
             this.classList.add('active')
         })
         obj.rate = this.innerText
-        main()
+        getFetch(obj)
     })
 })
 
-
-function main() {
-    getFetch(obj)  //yuxarida her defe main() chagirmaq yerine getFetch(obj) yoxla
-}
-function getFetch(obj) { 
-    // && obj.rate !== null
+function getFetch(obj) {
     if (obj.baseInput !== null) {
         fetch(`https://api.exchangerate.host/latest?base=${obj.base}&symbols=${obj.rate}`)
             .then(resp => resp.json())
             .then(data => {
-                // 1 obj.baseInput = obj.rate obj.rateInput
-                // console.log( "base amount : " + obj.baseInput * obj.rate / rateInput.value ) 
-
-                rate = Object.values(data.rates)[0] 
-                baseAmount = reValue / rate 
+                let amountOfRate = (obj.baseInput / rate).toFixed(3) //  for paragraph of rate
                 let amount = (rate * obj.baseInput).toFixed(2)
+                rate = Object.values(data.rates)[0]
                 rateInput.value = amount
-                let amountOfRate = (obj.baseInput / rate).toFixed(3) 
-                baseParagraph.innerText =  `1  ${obj.base} = ${rate.toFixed(3)} ${obj.rate}`
+
+                baseParagraph.innerText = `1  ${obj.base} = ${rate.toFixed(3)} ${obj.rate}`
                 rateParagraph.innerText = `1 ${obj.rate} = ${amountOfRate} ${obj.base}`
             })
-            .catch((err) => { 
-                alert(err);   
+            .catch((err) => {
+                console.warn(err);
             })
- 
-        }
     }
-    
-
-    function deneme(deger) {
-        console.log("deneme function : " + deger)
-    }
-    
-    
-    
-    
+}  
